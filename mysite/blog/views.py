dec3d00fd6dc04bf120b552e890388e7a4ae0a67
VToropov1337 <vladimirtoropov87@gmail.com>
2020-07-17 21:31:43 +0300

    save

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,19 +4,33 @@ from .models import Post
 from django.views.generic import ListView
 
 
-class PostListView(ListView):  
-    queryset = Post.published.all()  
-    context_object_name = 'posts'  
-    paginate_by = 1  
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 1
     template_name = 'blog/post/list.html'
 
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 1)  # По 3 статьи на каждой странице.
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
 
-def post_detail(request, year, month, day, post):  
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-			     status='published',  
-			     publish__year=year,  
-			     publish__month=month,  
-			     publish__day=day) 
-    return render(request,  
-		  'blog/post/detail.html',  
-		  {'post': post})
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
